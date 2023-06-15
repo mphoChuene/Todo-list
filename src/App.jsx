220974f6d2a6916413c9c67100cfd845fba11cf5
mphoChuene <mphochuene42@gmail.com>
2023-06-15 05:00:10 +0200

    server &  troubleshooting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+/* import "react-toastify/dist/react-toastify.css"; */
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 /* importing pages  */
 import TodoList from "./TodoList";
@@ -7,8 +10,39 @@ import Signup from "./components/signup/Signup";
 import Login from "./components/login/Login";
 
 export const App = () => {
+  const [displayusername, displayusernameupdate] = useState("");
+  const [showmenu, showmenuupdateupdate] = useState(false);
+  const usenavigate = useNavigate();
+  const location = useLocation();
+  useEffect(() => {
+    if (location.pathname === "/login" || location.pathname === "/register") {
+      showmenuupdateupdate(false);
+    } else {
+      showmenuupdateupdate(true);
+      let username = sessionStorage.getItem("username");
+      if (username === "" || username === null) {
+        usenavigate("/login");
+      } else {
+        displayusernameupdate(username);
+      }
+    }
+  }, [location]);
   return (
     <>
+      <div>
+        {showmenu && (
+          <div className="header">
+            <Link to={"/"}>Home</Link>
+            <Link to={"/customer"}>Customer</Link>
+            <span style={{ marginLeft: "70%" }}>
+              Welcome <b>{displayusername}</b>
+            </span>
+            <Link style={{ float: "right" }} to={"/login"}>
+              Logout
+            </Link>
+          </div>
+        )}
+      </div>
       {/*   routing of pages */}
       <Router>
         <Routes>
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -4,8 +4,10 @@ import profile from "../../components/image/a.png";
 import email from "../../components/image/email.jpg";
 import pass from "../../components/image/pass.png";
 
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 /* function Login({ handleLogin }) {
   const [email, setEmail] = useState("");
@@ -18,78 +20,143 @@ import { Link } from "react-router-dom";
     handleLogin(email);
   }; */
 const Login = () => {
+  const [username, usernameupdate] = useState("");
+  const [password, passwordupdate] = useState("");
+
+  const usenavigate = useNavigate();
+
+  useEffect(() => {
+    sessionStorage.clear();
+  }, []);
+
+  const ProceedLogin = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      ///implentation
+      // console.log('proceed');
+      fetch("http://localhost:8000/user/" + username)
+        .then((res) => {
+          return res.json();
+        })
+        .then((resp) => {
+          //console.log(resp)
+          if (Object.keys(resp).length === 0) {
+            toast.error("Please Enter valid username");
+          } else {
+            if (resp.password === password) {
+              toast.success("Success");
+              sessionStorage.setItem("username", username);
+              sessionStorage.setItem("userrole", resp.role);
+              usenavigate("/");
+            } else {
+              toast.error("Please Enter valid credentials");
+            }
+          }
+        })
+        .catch((err) => {
+          toast.error("Login Failed due to :" + err.message);
+        });
+    }
+  };
+
+  const ProceedLoginusingAPI = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      ///implentation
+      // console.log('proceed');
+      let inputobj = { username: username, password: password };
+      fetch("https://localhost:44308/User/Authenticate", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(inputobj),
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((resp) => {
+          console.log(resp);
+          if (Object.keys(resp).length === 0) {
+            toast.error("Login failed, invalid credentials");
+          } else {
+            toast.success("Success");
+            sessionStorage.setItem("username", username);
+            sessionStorage.setItem("jwttoken", resp.jwtToken);
+            usenavigate("/");
+          }
+          // if (Object.keys(resp).length === 0) {
+          //     toast.error('Please Enter valid username');
+          // } else {
+          //     if (resp.password === password) {
+          //         toast.success('Success');
+          //         sessionStorage.setItem('username',username);
+          //         usenavigate('/')
+          //     }else{
+          //         toast.error('Please Enter valid credentials');
+          //     }
+          // }
+        })
+        .catch((err) => {
+          toast.error("Login Failed due to :" + err.message);
+        });
+    }
+  };
+  const validate = () => {
+    let result = true;
+    if (username === "" || username === null) {
+      result = false;
+      toast.warning("Please Enter Username");
+    }
+    if (password === "" || password === null) {
+      result = false;
+      toast.warning("Please Enter Password");
+    }
+    return result;
+  };
   return (
-    <>
-      <div className="main">
-        <div className="sub-main">
-          <div>
-            <div className="imgs">
-              <div className="container-image">
-                <img src={profile} alt="profile" className="profile" />
-              </div>
+    <div className="row">
+      <div className="offset-lg-3 col-lg-6" style={{ marginTop: "100px" }}>
+        <form onSubmit={ProceedLogin} className="container">
+          <div className="card">
+            <div className="card-header">
+              <h2>User Login</h2>
             </div>
-            <div>
-              <h1>Login Page</h1>
-              <div>
-                <img src={email} alt="email" className="email" />
-                <input type="text" placeholder="user name" className="name" />
+            <div className="card-body">
+              <div className="form-group">
+                <label>
+                  User Name <span className="errmsg">*</span>
+                </label>
+                <input
+                  value={username}
+                  onChange={(e) => usernameupdate(e.target.value)}
+                  className="form-control"
+                ></input>
               </div>
-              <div className="second-input">
-                <img src={pass} alt="pass" className="email" />
+              <div className="form-group">
+                <label>
+                  Password <span className="errmsg">*</span>
+                </label>
                 <input
                   type="password"
-                  placeholder="password"
-                  className="name"
-                />
+                  value={password}
+                  onChange={(e) => passwordupdate(e.target.value)}
+                  className="form-control"
+                ></input>
               </div>
-              <div className="login-button">
-                <button>Login</button>
-              </div>
-
-              <p className="link">
-                <Link to={"/signup"}>Sign Up</Link>
-              </p>
+            </div>
+            <div className="card-footer">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>{" "}
+              |
+              <Link className="btn btn-success" to={"/register"}>
+                New User
+              </Link>
             </div>
           </div>
-        </div>
+        </form>
       </div>
-
-      {/*   animation screen */}
-      <div class="area">
-        <ul class="circles">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-    </>
+    </div>
   );
 };
-
-/*   <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div> */
 
 export default Login;
